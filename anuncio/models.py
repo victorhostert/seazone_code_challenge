a1c9a992c8d423cf5d231f5ff850ce5f5fe9914f
Victor W. Hostert <victorhostert@gmail.com>
2023-09-21 16:46:04 -0300

    Implementação dos Models

--- anuncio/models.py
+++ anuncio/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from imovel.models import Imovel
 
-# Create your models here.
+
+class Anuncio(models.Model):
+    imovel = models.ForeignKey(
+        verbose_name='Imóvel',
+        to=Imovel,
+        on_delete=models.CASCADE,
+    )
+    plataforma_nome = models.CharField(
+        verbose_name='Nome da Plataforma',
+        max_length=255,
+    )
+    plataforma_taxa = models.DecimalField(
+        verbose_name='Taxa da Plataforma',
+        decimal_places=2,
+        max_digits=10,
+    )
+    data_hora_criacao = models.DateTimeField(
+        verbose_name='Data e Hora de Criação',
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+    data_hora_atualizacao = models.DateTimeField(
+        verbose_name='Data e Hora da Última Atualização',
+        auto_now=True,
+    )
--- imovel/models.py
+++ imovel/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Imovel(models.Model):
+    codigo = models.AutoField(
+        verbose_name='Código do Imóvel',
+        auto_created=True,
+        unique=True,
+        primary_key=True,
+        editable=False,
+    )
+    limite = models.IntegerField(
+        verbose_name='Limite de Hóspedes',
+    )
+    qntd_banheiro = models.IntegerField(
+        verbose_name='Quantidade de banheiros',
+    )
+    animais = models.BooleanField(
+        verbose_name='Permissão para Animais de Estimação',
+        default=False,
+    )
+    valor_limpeza = models.DecimalField(
+        verbose_name='Valor para Limpeza',
+        decimal_places=2,
+        max_digits=10,
+    )
+    data_ativacao = models.DateField(
+        verbose_name='Data de Ativação',
+    )
+    data_hora_criacao = models.DateTimeField(
+        verbose_name='Data e Hora de Criação',
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+    data_hora_atualizacao = models.DateTimeField(
+        verbose_name='Data e Hora da Última Atualização',
+        auto_now=True,
+    )
